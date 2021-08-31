987a40e0e8bcd91ce59c959c4bc0c60a555ec643
jscarlson <jcarlson238@gmail.com>
2021-08-30 23:33:02 -0400

    Add total for tqdm

--- generate_personnel_segments.py
+++ generate_personnel_segments.py
@@ -152,7 +152,7 @@ def generate_personnel_blocks(
     model.load_networks(resume_iter)
 
     # generate blocks
-    for person_id, person_chars in tqdm(enumerate(personnel_entries)):
+    for person_id, person_chars in tqdm(enumerate(personnel_entries), total=count):
         for chars_label, chars in zip(personnel_entry_labels, person_chars):
 
             # create id
