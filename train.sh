911573cb66b48769689dbd618f46c5fc09907065
derwind <uncertainty_principle_ss@yahoo.co.jp>
2021-11-27 19:03:37 +0900

    fix channel size

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@
 CURRENT=$(cd $(dirname $0);pwd)
 cd $CURRENT
 
-screen -dmS zi2zi sh -c 'python train.py --experiment_dir=experiment --gpu_ids=cuda:0 --batch_size=32 --epoch=400 --sample_steps=5000 --checkpoint_steps=5000 2>&1 | tee $(date "+%Y%m%d_%H%M")_log.txt'
+screen -dmS zi2zi sh -c 'python train.py --experiment_dir=experiment --gpu_ids=cuda:0 --batch_size=32 --epoch=400 --sample_steps=5000 --checkpoint_steps=5000 --input_nc=1 2>&1 | tee $(date "+%Y%m%d_%H%M")_log.txt'
