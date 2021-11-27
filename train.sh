fddeb5f088a0026b8f2b7c53f944d5cdcd2ddc56
derwind <uncertainty_principle_ss@yahoo.co.jp>
2021-11-27 19:11:53 +0900

    fix how to log

--- train.sh
+++ train.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
 CURRENT=$(cd $(dirname $0);pwd)
-cd $CURRENT
 
-screen -dmS zi2zi sh -c 'python train.py --experiment_dir=experiment --gpu_ids=cuda:0 --batch_size=32 --epoch=400 --sample_steps=5000 --checkpoint_steps=5000 --input_nc=1 2>&1 | tee $(date "+%Y%m%d_%H%M")_log.txt'
+(cd $CURRENT && screen -dmS zi2zi sh -c 'python train.py --experiment_dir=experiment --gpu_ids=cuda:0 --batch_size=32 --epoch=400 --sample_steps=5000 --checkpoint_steps=5000 --input_nc=1 2> $(date '+%Y%m%d_%H%M')_log.txt')
